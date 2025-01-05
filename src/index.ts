82d10ebe6a5e8e263f0ab82cb07098ac26544284
loidpadre <loidpadre@gmail.com>
2025-01-05 22:51:55 +0000

    configure 1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import router from './routes/routes'
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 10000
 const app = express()
 app.use(express.json())
 
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', router)
-app.listen(PORT, () => {
+app.listen(port, () => {
 	console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
